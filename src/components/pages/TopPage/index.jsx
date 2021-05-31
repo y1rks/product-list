e7439f92eb60489a1e9b8b3c3ef2a5a993b41526
y1rks <yu.martian@gmail.com>
2021-05-31 22:14:33 +0900

    Add search

--- src/components/pages/TopPage/index.jsx
+++ src/components/pages/TopPage/index.jsx
@@ -1,13 +1,24 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
+const SEARCH_URL = 'http://localhost:3000/api/search?q=';
+
+function fetchProducts(event, setPosts) {
+  const url = SEARCH_URL + event.target.value;
+  fetch(url, {method: 'GET'}).then(async (res) => {
+    return res.json();
+  }).then(data => {
+    console.log(data);
+    setPosts(data)
+  });
+}
+
 const TopPage = () => {
 
-  const [posts, setPosts] = useState([])
-  const url = 'http://localhost:3000/api/confirm'
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    fetch(url, {method: 'GET'})
+    fetch(SEARCH_URL, {method: 'GET'})
     .then(async (res) => {
       return res.json()
     })
@@ -21,6 +32,9 @@ const TopPage = () => {
       <h1>Products List</h1>
       <Link to="/play/hoge">Player Page</Link>
       <p>---------------------</p>
+      <div>
+        <input type="text" onChange={e => {fetchProducts(e, setPosts)}}></input>
+      </div>
       <div>
         <ul>
           {
